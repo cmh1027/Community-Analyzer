6a5de3357627b586c6605a660358908f9246b689
unknown <sodlqnf123@naver.com>
2021-11-15 17:54:56 +0900

    remove function

--- doc2vec.py
+++ doc2vec.py
@@ -6,24 +6,6 @@ import torch
 from utility import constant
 from tqdm import trange
 
-def getMergedDocVec(formats, weight=torch.tensor([1.0, 1.0, 1.0])):
-    format_vectors = []
-    for format in formats:
-        with open("model/d2v_"+format+".model.w2v_format") as f:
-            u = f.readline()
-            number = u.split(" ")[0]
-            vectors = []
-            for _ in trange(int(number), desc=format+" processing..."):
-                vectors.append([float(i) for i in f.readline().split()[1:]])
-            format_vectors.append(vectors)
-    format_vectors = torch.tensor(format_vectors)
-    n, p, d = format_vectors.shape
-    for i in trange(n, desc="standardization..."): # standardization
-        format_vectors[i] = (format_vectors[i] - torch.mean(format_vectors[i])) / torch.var(format_vectors)
-    format_vectors = (format_vectors.view(n, -1) * weight[:, None]).view(n, p, d)
-    merged_vector = torch.cat(format_vectors.split(1)[:], dim=2).squeeze()
-    return merged_vector
-
 if __name__ == "__main__":
     for format in constant.FORMATS:
         tokenized = json.load(open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "data/" + format + "_tokenized.json"), 'r'))
